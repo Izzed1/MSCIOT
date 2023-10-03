7aa028cbabf905223f546af990656f0149c52556
ardian-arif-wahyudi <ardiarif72@gmail.com>
2023-10-03 14:05:16 +0700

    semifinal

--- app/src/main/java/com/unissula/msciot/retrofit/ApiService.kt
+++ app/src/main/java/com/unissula/msciot/retrofit/ApiService.kt
@@ -3,12 +3,15 @@ package com.unissula.msciot.data.retrofit
 import com.unissula.msciot.data.TrackData
 import com.unissula.msciot.data.TrackDataResponse
 import com.unissula.msciot.data.retrofit.*
-import retrofit2.Call
 import retrofit2.http.*
 
 
 interface ApiService {
-    @GET("api/UserModels/1")
-    suspend fun getDataTracker(): TrackDataResponse
+        @GET("api/UserModels/1")
+        suspend fun getDataTracker(): TrackDataResponse
+
+        @GET("api/UserModels/")
+        suspend fun getAllDataTracker(): List<TrackData>
+
 
 }
